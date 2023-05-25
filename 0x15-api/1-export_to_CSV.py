817cf0d077e6252d631289395c9c6f59b36827a8
BLACKMouha <mohamedsenghor05@gmail.com>
2023-05-25 12:07:51 +0000

    Export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''1-export_to_CSV module'''
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+
+    if len(argv) == 2:
+
+        try:
+            q = int(argv[1])
+        except Exception as e:
+            raise Exception
+        h = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        u = 'https://jsonplaceholder.typicode.com/users/' + str(q)
+        r = requests.get(u)
+        n = r.json().get('username', None)
+
+        u = 'https://jsonplaceholder.typicode.com/todos/'
+        r = requests.get(u)
+        d = []
+        td = 0
+        for k in r.json():
+            i = []
+            if q == k.get('userId', None):
+                i.append(k.get('userId', None))
+                i.append(n)
+                i.append(k.get('completed', None))
+                i.append(k.get('title', None))
+            d.append(i)
+            if k.get('userId') > q:
+                break
+
+        with open('{}.csv'.format(q), 'w', newline='') as f:
+            csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            csv_writer.writerow(h)
+            csv_writer.writerows(d)

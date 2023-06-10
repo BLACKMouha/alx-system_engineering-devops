6105f63666699747a8f0062aacf76b30e2561e4e
BLACKMouha <mohamedsenghor05@gmail.com>
2023-06-10 19:34:02 +0000

    Fectches the first ten host posts of a subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,14 +8,14 @@ from itertools import islice
 def top_ten(subreddit):
     '''Queries the Reddit API and returns the first ten host posts listed
     for a given subreddit'''
-    if (subreddit is None):
+    if (subreddit is None or subreddit == ''):
         print(None)
         return
     req = "https://www.reddit.com/r/{}.json".format(subreddit)
-    res = requests.get(req)
-    if (res.status_code == 404):
+    res = requests.get(req, all_redirects=False)
+    if (str(res.status_code)[0] != '2'):
         print(None)
         return
     first_ten_posts = list(islice(res.json()['data']['children'], 10))
-    for k in first_ten_posts:
+    for k in sorted(first_ten_posts):
         print(k['data']['title'])

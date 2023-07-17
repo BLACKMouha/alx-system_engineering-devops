57f025e986342be75cbfd0eb30cebc89085e4819
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-17 09:18:43 +0000

    Puppet script that fixes a bug in a WordPress website

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # Puppet script that fixes a bug in a WordPress website
-$wp__setttings = '/var/www/html/wp-settings.php'
 
 exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${wp__settings}",
-  path    => ['/bin','/usr/bin']
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }

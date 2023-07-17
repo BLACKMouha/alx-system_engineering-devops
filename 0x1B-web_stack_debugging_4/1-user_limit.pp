f84c27a5ea31a999e3d6f14c77e8b8e389394962
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-17 10:45:21 +0000

    Execute the command as sudo

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,5 +2,5 @@
 
 exec { 'increase_os_worker_files':
   command => 'sudo sed -ie "$a\fs.file-max = 65536" /etc/sysctl.conf'
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  path    => '/usr/local/bin/:/bin/',
 }
